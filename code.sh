8a665c2a93936df8f687bce3c372899f10a59b39
ManarRashad <48625774+ManarRashad@users.noreply.github.com>
2019-05-11 03:49:40 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -21,22 +21,18 @@ samtools sort bowtie2.bam -o SRR8115017.sorted.bam
 picard_path=$CONDA_PREFIX/share/picard-2.19.0-0
 
 java -Xmx2g -jar $picard_path/picard.jar MarkDuplicates INPUT=SRR8115017.sorted.bam OUTPUT=SRR8115017.dedup.bam METRICS_FILE=SRR8115017.metrics.txt
-Indexing
 
+#Indexing
 java -Xmx2g -jar $picard_path/picard.jar BuildBamIndex VALIDATION_STRINGENCY=LENIENT INPUT=SRR8115017.dedup.bam
-
 java -Xmx2g -jar $picard_path/picard.jar CreateSequenceDictionary R=Homo_sapiens.GRCh38.dna.chromosome.21.fa O=Homo_sapiens.GRCh38.dna.chromosome.21.dict
-
 samtools faidx Homo_sapiens.GRCh38.dna.chromosome.21.fa
 
 #Downloading known variants:
 wget ftp://ftp.ensembl.org/pub/release-96/variation/vcf/homo_sapiens/homo_sapiens-chr21.vcf.gz
-
 gunzip homo_sapiens-chr21.vcf.gz 
 
 #Indexing:
 gatk IndexFeatureFile -F Homo_sapiens_chr21.vcf
-
 head -1291601 Homo_sapiens_chr21.vcf | tail -1   #Then remove this record 
 
 #Base recalibration:
